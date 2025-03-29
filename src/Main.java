c381438182b4640def6ee7bf481ec81a8c356ad2
Pyaesone Khant <pyaepsk@gmail.com>
2025-03-29 13:27:28 +0630

    number guessing game

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,40 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-//		switch statement
-		String day;
+//		number guessing game
 
+		Random rand = new Random();
 		Scanner sc = new Scanner(System.in);
 
-		System.out.print("Please enter the day of the week: ");
-		day = sc.nextLine();
+		int guess;
+		int attempts = 0;
+		int min = 1;
+		int max = 100;
+		int randomNumber = rand.nextInt(min, max + 1);
 
-		System.out.println("Output from switch case 1: =>");
-//		switch-case 1
-		switch (day) {
-			case "Monday", "Tuesday", "Wednesday", "Thursday", "Friday":
-				System.out.println("It's a weekday! 🤨");
-				break;
-			case "Saturday", "Sunday":
-				System.out.println("Yay! It's a weekend! 🤣");
-				break;
-			default:
-				System.out.println(day + " is not a day!");
-		}
+		System.out.println("Number Guessing Game!");
+		System.out.printf("Please enter a number between %d - %d \n", min, max);
 
-		System.out.println("Output from switch case 2: =>");
-//		switch-case 2
-		switch (day) {
-			case "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" -> System.out.println("It's a weekday! 🤨");
-			case "Saturday", "Sunday" -> System.out.println("Yay! It's a weekend! 🤣");
-			default -> System.out.println(day + " is not a day!");
-		}
+		do {
+			System.out.print("Enter a guess: ");
+			guess = sc.nextInt();
+			attempts++;
+
+			if (guess < randomNumber) {
+				System.out.println("Your number is too low! Please try again.");
+			} else if (guess > randomNumber) {
+				System.out.println("Your number is too high! Please try again.");
+			} else {
+				System.out.println("Correct! Your number is " + guess);
+				System.out.println("Total Number of Attempts #: " + attempts);
+			}
+		} while (guess != randomNumber);
+
+		System.out.printf("You have won with the total number of attempts: %d!\n", attempts);
 
 		sc.close();
 	}
