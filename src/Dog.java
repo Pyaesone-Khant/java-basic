c92cc0a904313e5c472f021d86dc9b17de143c4f
Pyaesone Khant <pyaepsk@gmail.com>
2025-03-31 16:43:26 +0630

    inheritance in java

---
+++ src/Dog.java
@@ -0,0 +1,12 @@
+public class Dog extends Animal {
+
+//	Inheritance => one class inherits the attributes and methods
+//				   from another class.
+//		           GrandParent -> Parent -> Child
+
+	int lives = 1;
+
+	void bark() {
+		System.out.println("The dog barks!");
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,14 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Friend friend = new Friend("John");
-		System.out.println(friend.numOfFriends);
+		Dog dog = new Dog();
+		Cat cat = new Cat();
 
-		Friend friend2 = new Friend("James");
-		System.out.println(friend2.numOfFriends);
+		Plant plant = new Plant();
 
-		Friend friend3 = new Friend("James");
-		System.out.println(friend3.numOfFriends);
+		System.out.println(dog.lives);
+		System.out.println(cat.lives);
 
-		Friend friend4 = new Friend("James");
-		System.out.println(friend4.numOfFriends);
-
-		Friend.showFriends();
+		plant.photosynthesize();
 	}
 }
