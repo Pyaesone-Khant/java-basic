f104b0b9937429827b81c1fec996112f881e2164
Pyaesone Khant <pyaepsk@gmail.com>
2025-03-31 15:02:26 +0630

    constructor in java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Car car = new Car();
+		Student student1 = new Student("John", 25, "Computer Science");
+		Student student2 = new Student("Jane", 23, "Electrical Engineering");
 
-		car.start();
-		car.drive();
-		car.brake();
-		car.stop();
+		System.out.println("Student 1 => ");
+		System.out.println(student1.name);
+		System.out.println(student1.age);
+		System.out.println(student1.major);
+
+		System.out.println("\nStudent 2 => ");
+		System.out.println(student2.name);
+		System.out.println(student2.age);
+		System.out.println(student2.major);
 	}
 }
---
+++ src/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+
+	String name;
+	int age;
+	String major;
+
+	//	constructor => a special method to initialize objects. You can pass arguments to a constructor and set up initial values.
+	Student(String name, int age, String major) {
+		this.name = name;
+		this.age = age;
+		this.major = major;
+	}
+}
