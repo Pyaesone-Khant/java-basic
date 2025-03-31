a3b5e0000f22366016b03ba7973c7eee72367fda
Pyaesone Khant <pyaepsk@gmail.com>
2025-03-31 11:57:02 +0630

    rock-paper-scissor game

--- src/Main.java
+++ src/Main.java
@@ -1,72 +1,76 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-//		Java Quiz Game
+//		ROCK - PAPER - SCISSORS GAME
 
-//		1. questions array[]
-//		2. options array[]
-//		3. declare variables
-//		4. welcome message
-//		5. question (loop)
-//		    1. options (loop)
-//		    2. get guess from user
-//		    3. check our guess
-//		6. display final score
-
-		String[] questions = {
-				"What is the capital of France?",
-				"What is the capital of Germany?",
-				"What is the capital of Italy?",
-				"What is the capital of Spain?",
-				"What is the capital of Myanmar?"
-		};
-
-		String[][] options = {
-				{"1. Paris", "2. Berlin", "3. London", "4. Rome"},
-				{"1. Madrid", "2. Barcelona", "3. Vienna", "4. Berlin"},
-				{"1. Empoli", "2. Milan", "3. Rome", "4. Turin"},
-				{"1. Betis", "2. Madrid", "3. Vienna", "4. Rome"},
-				{"1. Naypyidaw", "2. Kayah", "3. Myanmar", "4. Shan"}
-		};
-
-		int[] answers = {1, 4, 3, 2, 1};
-		int score = 0;
-		int guess;
+//		1. declare vars
+//		2. get choice from the user
+//		3. get random choice for the computer
+//		4. check win condition
+//		5. ask to play again?
+//		6. goodbye message
 
 		Scanner scanner = new Scanner(System.in);
+		Random random = new Random();
 
-		System.out.println("--------------------------------");
-		System.out.println("Welcome to Java Quiz Game!");
-		System.out.println("--------------------------------");
+//		1. =>
+		String[] choices = {"ROCK", "PAPER", "SCISSORS"};
+		String playerChoice;
+		String computerChoice;
+		String playAgain = "YES";
 
-		for (int i = 0; i < questions.length; i++) {
-			System.out.println(questions[i]);
+		do {
+			System.out.println("------------------------------------------");
+			System.out.println("Welcome to the Rock, Paper, Scissors game!");
+			System.out.println("------------------------------------------");
 
-			for (String option : options[i]) {
-				System.out.println(option);
-			}
+			//		2. =>
+			playerChoice = getUserChoice(scanner);
 
-			System.out.print("Enter your guess: ");
-			guess = scanner.nextInt();
+//			3. =>
+			computerChoice = choices[random.nextInt(choices.length)];
+			System.out.println("Computer choice: " + computerChoice);
 
-			if (guess == answers[i]) {
-				System.out.println("********");
-				System.out.println("CORRECT!");
-				System.out.println("********");
-				score++;
+//			4. =>
+			System.out.println("************");
+			if (computerChoice.equals(playerChoice)) {
+				System.out.println("It's a tie!");
+			} else if (checkWin(playerChoice, computerChoice)) {
+				System.out.println("You win!");
 			} else {
-				System.out.println("********");
-				System.out.println(" WRONG! ");
-				System.out.println("********");
+				System.out.println("You lose!");
 			}
-		}
+			System.out.println("************");
 
-		System.out.println("--------------------------------");
-		System.out.println("Your total score is: " + score + " out of " + questions.length);
-		System.out.println("--------------------------------");
+//			5. =>
+			System.out.print("Do you want to play again? (yes/no): ");
+			playAgain = scanner.nextLine().toUpperCase();
+		} while (playAgain.equals("YES"));
+
+//		6. =>
+		System.out.println("Goodbye! See you next time!");
 
 		scanner.close();
+	}
+
+	static boolean checkWin(String playerChoice, String computerChoice) {
+		return playerChoice.equals("ROCK") && computerChoice.equals("SCISSORS") ||
+				playerChoice.equals("PAPER") && computerChoice.equals("ROCK") ||
+				playerChoice.equals("SCISSORS") && computerChoice.equals("PAPER");
+	}
+
+	static String getUserChoice(Scanner scanner) {
+		System.out.print("Please enter your choice (ROCK, PAPER, SCISSORS): ");
+		String userChoice = scanner.nextLine().toUpperCase();
+
+		if (!userChoice.equals("ROCK") && !userChoice.equals("PAPER") && !userChoice.equals("SCISSORS")) {
+			System.out.println("Invalid choice!");
+			return getUserChoice(scanner);
+		}
+
+		return userChoice;
 	}
 }
