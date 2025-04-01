f15194a0750a1f6a45cd0159c8ae7fa25ff39feb
Pyaesone Khant <pyaepsk@gmail.com>
2025-04-01 13:54:24 +0630

    interface in java

--- src/Fish.java
+++ src/Fish.java
@@ -1,11 +1,16 @@
-public class Fish extends Animal {
+public class Fish implements Prey, Predator {
 
 //	method overriding => when a subclass override its own implementation of a method that is already defined.
 //	                     allows for code reusability and give specific implementation.
 
 	@Override
-	void move() {
-		System.out.println("This animal is swimming!");
+	public void flee() {
+		System.out.println("This fish is swimming away!");
+	}
+
+	@Override
+	public void hunt() {
+		System.out.println("The fish is hunting!");
 	}
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
 public class Main {
 
 	public static void main(String[] args) {
+//		interface => a blueprint for a class that specifies a set of abstract methods
+//		          => that implementing classes MUST define.
+//		          => supports multiple inheritance-like behavior
 
-		Circle circle = new Circle(10);
-		Triangle triangle = new Triangle(4, 5);
-		Rectangle rectangle = new Rectangle(10, 4);
+		Rabbit rabbit = new Rabbit();
+		Hawk hawk = new Hawk();
+		Fish fish = new Fish();
 
-		System.out.println(circle.area());
-		System.out.println(triangle.area());
-		System.out.println(rectangle.area());
+		fish.flee();
+		fish.hunt();
 	}
 }
