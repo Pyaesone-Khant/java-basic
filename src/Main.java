bae6e00d5a07d841609599dac3fd4e939ffd6eb8
Pyaesone Khant <pyaepsk@gmail.com>
2025-04-24 10:14:16 +0630

    FileWriter

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,36 @@
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) {
-//        Exception = an event that interrupts the normal flow of a program
-//                    (Dividing by zero, file not found, mismatch input type)
-//                    Surrounded any dangerous code with a try{} block
-//                    try{}, catch{}, finally{}
+	public static void main(String[] args) {
+//        how to write a file using java (4 popular ways)
 
-        Scanner scanner = new Scanner(System.in);
+//        1. FileWriter => good for small/medium-sized text files
+//        2. BufferedWriter => better performance for large amounts of text
+//        3. PrintWriter => best for structured data, like reports or logs
+//        4. FileOutputStream => best for binary files (eg: images, audio files)
 
-        try {
-            System.out.print("Enter a number: ");
-            int number = scanner.nextInt();
-            System.out.println(number);
-        } catch (InputMismatchException e) {
-            System.out.println("THAT WASN'T A NUMBER!");
-        } catch (ArithmeticException e) {
-            System.out.println("YOU CAN'T DIVIDE BY ZERO!");
-        } catch (Exception e) {
-//            SAFETY NET
-            System.out.println("SOMETHING WENT WRONG!");
-        } finally {
-            scanner.close();
-            System.out.println("THIS ONE WILL ALWAYS BE EXECUTED!");
-        }
-    }
 
+//		FileWriter Example
+
+		String filePath = "text.txt"; // can also give absolut file path (eg: C:\Users\hp user\Desktop\)
+		String text = """
+				I like yours Sketchers
+				You like me, my Gucci Shoes
+				I'll buy you the purse
+				If you only show me your BooBs
+				""";
+
+		try (FileWriter fileWriter = new FileWriter(filePath)) {
+			fileWriter.write(text);
+			System.out.println("File written!");
+		} catch (FileNotFoundException e) {
+			System.out.println("Could not locate file location!.");
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+			System.out.println("Something went wrong while writing to the file!");
+		}
+	}
 }
