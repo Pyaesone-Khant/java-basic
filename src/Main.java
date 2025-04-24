d4d9f5eb1c173c9dceeaeb2c5ab613992e1e50da
Pyaesone Khant <pyaepsk@gmail.com>
2025-04-24 10:55:46 +0630

    Java Audio Player

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,54 @@
-import java.io.BufferedReader;
+import javax.sound.sampled.*;
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-//        how to read a file using Java (3 popular ways)
+//        how to play Audio with JAVA (.wav, .au, .aiff)
 
-//		1. BufferedReader + FileReader => best for reading text files line-by-line
-//		2. FileInputStream => best for binary files (eg: images, audio files)
-//		3. RandomAccessFile => best for read/writer specific portions of a large file.
+		String filePath = "src\\The Last Bit Of Us.wav";
 
-		String filePath = "text.txt";
+		File file = new File(filePath);
 
+		try (
+				Scanner scanner = new Scanner(System.in);
+				AudioInputStream ais = AudioSystem.getAudioInputStream(file)
+		) {
+			Clip clip = AudioSystem.getClip();
+			clip.open(ais);
 
-		try (BufferedReader br = new BufferedReader(new FileReader(filePath));) {
-			System.out.println("File found!");
-			String line;
-			while ((line = br.readLine()) != null) {
-				System.out.println(line);
+			String response = "";
+			while (!response.equals("Q")) {
+				System.out.println("P = Play");
+				System.out.println("S = Stop");
+				System.out.println("R = Reset");
+				System.out.println("Q = Quit");
+				System.out.print("Enter your choice: ");
+
+				response = scanner.nextLine().toUpperCase();
+
+				switch (response) {
+					case "P" -> clip.start();
+					case "S" -> clip.stop();
+					case "R" -> clip.setMicrosecondPosition(0);
+					case "Q" -> clip.close();
+					default -> System.out.println("Invalid choice!");
+				}
 			}
 		} catch (FileNotFoundException e) {
-			System.out.println("File not found!");
+			System.out.println("File Not Found!");
+		} catch (UnsupportedAudioFileException e) {
+			System.out.println("Audio Format not supported");
+		} catch (LineUnavailableException e) {
+			System.out.println("Unable to access the audio resource!");
 		} catch (IOException e) {
-			throw new RuntimeException(e);
+			System.out.println(e.getMessage());
+			System.out.println("Something went wrong!");
+		} finally {
+			System.out.println("Done!");
 		}
-
 	}
 }
